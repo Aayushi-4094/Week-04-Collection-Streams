8290737f1c5a5eb77d43623caa4e7b5277e5ecc8
Aayushi <aayushiagarwal4094@gmail.com>
2025-04-29 14:23:46 +0530

    Added word frequency counter

--- day1/Pro15.java
+++ day1/Pro15.java
@@ -1,54 +1,28 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Pro15{
-    public static void main(String[] args) throws IOException {
-        Path filePath = Paths.get("myFile.txt");
-
-        Map map = new HashMap();
+public class Pro15 {
+    public static void main(String[] args) throws IOException {        Map<String, Integer> wordFrequency = new HashMap<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader("myFile.txt"))) {
-            StringBuilder sb = new StringBuilder();
-            StringBuffer content = new StringBuffer();
-            String line = br.readLine();
-
-            while (line != null) { 
-                
-                
-                String[] words = line.split(" ");
-                
-                for (String word : words){
-                    if(word == null || word.trim().equals("")){
-                        continue;
-                        
-                    }                
-                }
-                
-               
-                for (int i = 0; i < words.length; i++) {
-                    
-                    if (map.get(words[i]) == null) {
-                        map.put(words[i], 1);
-                    } else {
-                        int newValue = Integer.valueOf(String.valueOf(map.get(words[i])));
-                        newValue++;
-                        map.put(words[i], newValue);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.toLowerCase().replaceAll("[^a-zA-Z ]", "").split("\\s+");
+                for (String word : words) {
+                    if (!word.isEmpty()) {
+                        wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
                     }
                 }
-                sb.append(System.lineSeparator());
-                line = br.readLine();
             }
         }
-        Map<String, Integer> sorted = new TreeMap<String, Integer>(map);
-        for (Object key : sorted.keySet()) {
-            System.out.println(key + "\tCounts: " + map.get(key));
+
+        Map<String, Integer> sorted = new TreeMap<>(wordFrequency);
+        for (Map.Entry<String, Integer> entry : sorted.entrySet()) {
+            System.out.println(entry.getKey() + " = "+ entry.getValue());
         }
     }
-    
 }
