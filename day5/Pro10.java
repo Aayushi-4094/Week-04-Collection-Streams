8b4d3d73ad958fe66659d6aa18fe3915ffcfccab
Aayushi <aayushiagarwal4094@gmail.com>
2025-05-08 10:33:02 +0530

    Valid ip address

---
+++ day5/Pro10.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Pro10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the IP address");
+        String ip = sc.nextLine();
+        String regex = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(ip);
+        if (matcher.matches()) {
+            System.out.println("Valid IP address");
+        } else {
+            System.out.println("Invalid IP address");
+        }
+    }
+}
+
